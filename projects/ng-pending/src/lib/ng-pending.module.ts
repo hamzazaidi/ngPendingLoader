607273bb36bfda6998aa4ec9f2b0da95028cd5d5
Zaidi,Hamza <hamza_zaidi@vanguard.com>
2018-12-21 22:23:33 -0500

    exposes loaders for customization

--- projects/ng-pending/src/lib/ng-pending.module.ts
+++ projects/ng-pending/src/lib/ng-pending.module.ts
@@ -3,6 +3,13 @@ import { NgPendingDirective } from './directive/ng-pending.directive';
 import { BouncingLoaderComponent } from './components/bouncing-loader/bouncing-loader.component';
 import { BouncingStringsComponent } from './components/bouncing-strings/bouncing-strings.component';
 import { RotatingSquareComponent } from './components/rotating-square/rotating-square.component';
+import { ILoaders } from './models/loaders';
+
+export const Loaders: ILoaders = {
+  bouncingLoader: BouncingLoaderComponent,
+  bouncingString: BouncingStringsComponent,
+  rotatingSquare: RotatingSquareComponent
+};
 
 @NgModule({
   declarations: [ NgPendingDirective, BouncingLoaderComponent, BouncingStringsComponent, RotatingSquareComponent ],
