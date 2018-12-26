bfc5910b37d85dc6cb75d99cf43874761388fc4e
Zaidi,Hamza <hamza_zaidi@vanguard.com>
2018-12-26 15:54:37 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,25 +12,25 @@ export class AppComponent implements OnInit {
   message$: Observable<string>;
   users = [
     {
-      'id': 4,
-      'first_name': 'Eve',
-      'last_name': 'Holt',
-      'avatar': 'https://s3.amazonaws.com/uifaces/faces/twitter/marcoramires/128.jpg',
-      'isProcessing': false
+      id: 4,
+      first_name: 'Eve',
+      last_name: 'Holt',
+      avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/marcoramires/128.jpg',
+      isProcessing: false
     },
     {
-      'id': 5,
-      'first_name': 'John',
-      'last_name': 'Doe',
-      'avatar': 'https://s3.amazonaws.com/uifaces/faces/twitter/stephenmoon/128.jpg',
-      'isProcessing': true
+      id: 5,
+      first_name: 'John',
+      last_name: 'Doe',
+      avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/stephenmoon/128.jpg',
+      isProcessing: true
     },
     {
-      'id': 6,
-      'first_name': 'Tracey',
-      'last_name': 'Ramos',
-      'avatar': 'https://s3.amazonaws.com/uifaces/faces/twitter/bigmancho/128.jpg',
-      'isProcessing': false
+      id: 6,
+      first_name: 'Tracey',
+      last_name: 'Ramos',
+      avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/bigmancho/128.jpg',
+      isProcessing: false
     }
   ];
 
@@ -39,3 +39,4 @@ export class AppComponent implements OnInit {
   }
 
 }
+
